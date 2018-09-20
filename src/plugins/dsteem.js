01bb113f6ee42bf64feebe5eb6d0170287daec50
nothingismagick <35242872+nothingismagick@users.noreply.github.com>
2018-09-21 00:22:12 +0200

    Revert "Nothing (#8)" (#9)
    This reverts commit 20301b34db251c0d924c4b388bba7e16e37f8432.

--- src/plugins/dsteem.js
+++ src/plugins/dsteem.js
@@ -1,9 +1,8 @@
 import { Client } from 'dsteem'
 
 export default ({ app, router, Vue, store }) => {
-  // Vue.prototype.$steemClient = new Client('https://api.steemit.com')
-  Vue.prototype.$steemClient = new Client('https://testnet.steem.vc', {
-    addressPrefix: 'STX',
-    chainId: '79276aea5d4877d9a25892eaa01b0adf019d3e5cb12a97478df3298ccdd01673'
-  })
+  const steemClient = new Client('https://api.steemit.com')
+  Vue.prototype.$steemClient = steemClient
+  // let dsteem = new Client('https://api.steemit.com')
+  // store.commit('steem/dsteem', dsteem)
 }
