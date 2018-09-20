264d1f7ca918cc329d37926100b36c2dd77bfba3
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-09-21 05:48:42 +0700

    feat(sync): add push and fetch Button 🎉
    TODO: now let me create some usage diagram and put it to PR message 😉

---
+++ src/components/git-notes/FetchButton.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  q-btn(icon="cloud_download" v-bind="$attrs" @click="fetch")
+</template>
+
+<style>
+</style>
+
+<script>
+import { fetch } from './index'
+
+/** A button to fetch/pull notes
+ * @prop {String} [workingDirectory=process.cwd()] in case the application not runned in project directory
+ * @fires String#success when notes is succesfully fetched/pulled/downloaded/updated
+ * @fires String#fail when fetch/pull/download/update notes fail
+ * @example gn-fetch-button(workingDirectory="/home/user/projects")
+ */
+export default {
+  name: 'GitNotesFetchButton',
+  props: {
+    workingDirectory: String
+  },
+  methods: {
+    fetch () {
+      fetch(this.workingDirectory)
+        .then(data => this.$emit('success', data))
+        .catch(data => this.$emit('fail', data.toString()))
+    }
+  }
+}
+</script>
