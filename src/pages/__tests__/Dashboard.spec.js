c0230592ee2ab2ab08c191b3514a931ca87c820f
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-10-28 14:21:38 +0700

    test: fix test suites for MainLayout & Dashboard 🔧

--- src/pages/__tests__/Dashboard.spec.js
+++ src/pages/__tests__/Dashboard.spec.js
@@ -1,11 +1,12 @@
 import { mountQuasar } from '~/test/utils'
 import i18nPlugin from '@/plugins/i18n'
+import markdownPlugin from '@/plugins/marked'
 import PageDashboard from '@/pages/Dashboard.vue'
 
 describe('Dashboard.vue', () => {
   test('init', () => {
     mountQuasar(PageDashboard, {
-      plugins: [i18nPlugin]
+      plugins: [i18nPlugin, markdownPlugin]
     })
   })
 })
