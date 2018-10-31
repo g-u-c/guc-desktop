405fd5e513924d230a44d6729a6bdf8fcd589429
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-10-31 14:32:48 +0700

    chore: remove `git notes show` helper function 😋

--- src/components/git-notes/index.js
+++ src/components/git-notes/index.js
@@ -1,25 +1,5 @@
 /** */
-import { run, runSync } from '@/node-utils'
-
-/** A button to insert notes
- * @param {String} cwd Current working directory where git command is executed in case the desktop application not runned in the project directory
- * @param {String} sha SHA-1 of commits. If not provided, it will show notes in the HEAD (current commit)
- * @return {Promise<resolve:String, reject:String>} stored notes
- * @example
- *
- * async read() {
- *  try {
- *    this.notes = await show(this.workingDirectory)
- *  } catch(e) { this.toast(e) }
- * }
- */
-export const show = async (cwd, sha) => {
-  const getHead = [runSync('git', ['rev-parse', 'HEAD'])]
-  try {
-    const result = await run('git', ['notes', '--ref=utopian', 'show', ...(sha ? [sha] : getHead)], { cwd })
-    return result.stdout
-  } catch (error) { throw error }
-}
+import { run } from '@/node-utils'
 
 export const push = async (cwd) => run('git', ['push', 'origin', 'refs/notes/utopian'], { cwd })
 export const fetch = async (cwd) => {
